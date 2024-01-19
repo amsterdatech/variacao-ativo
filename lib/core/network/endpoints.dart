7966c9b291dc7701916b5e115a64929531060498
William Gouvea <williamg@backbase.com>
2024-01-19 14:58:09 -0300

    -Add riverpod, freezed, dio libraries -Create dio_client -Create constants file endpoints to network layer

---
+++ lib/core/network/endpoints.dart
@@ -0,0 +1,13 @@
+class Endpoints {
+  // base url
+  static const String baseUrl =
+      "https://query2.finance.yahoo.com/v8/finance/chart/";
+
+  static const String currentStock = 'PETR4.SA';
+
+  // receiveTimeout
+  static const int receiveTimeout = 15000;
+
+  // connectTimeout
+  static const int connectionTimeout = 30000;
+}
